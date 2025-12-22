cdfcedc9a6e28586b115f2f1b520fe5f69391138
surajgm-coder <suraj.gm@nkenters.com>
2025-12-22 12:11:21 +0530

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("common.html")
+    .then(response => response.text())
+    .then(data => {
+      document.body.insertAdjacentHTML("afterbegin", data);
+    });
+});
 
