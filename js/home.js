861fcd7c7e6d95afcd6c65bdcefe676ecb6b1cda
surajgm-coder <suraj.gm@nkenters.com>
2025-12-23 14:06:37 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,13 +1,1 @@
-const slides = document.querySelectorAll(".slide");
-const dots = document.querySelectorAll(".dot");
-let current = 0;
 
-setInterval(() => {
-  slides[current].classList.remove("active");
-  dots[current].classList.remove("active");
-
-  current = (current + 1) % slides.length;
-
-  slides[current].classList.add("active");
-  dots[current].classList.add("active");
-}, 5000);
